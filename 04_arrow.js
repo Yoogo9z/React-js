560a4c3c7860e1d6c1f0e8cb05732c34c638f045
Hugo Cnt <113591086+Yoogo9z@users.noreply.github.com>
2024-05-13 14:32:33 +0200

    exercice 4

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,8 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (a) => {return a+1};
+console.log(arrow1(3));
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
@@ -18,6 +19,8 @@ let arrow1 = () => {}
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = (b) => b+1;
+let resultat = arrow2(8);
+console.log(resultat);
 
-module.exports = {arrow1, arrow2};
+// module.exports = {arrow1, arrow2};
